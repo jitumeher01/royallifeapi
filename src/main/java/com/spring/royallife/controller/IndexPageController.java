af981111ce25dc4ca099203c35ba13037a3e64cc
Jitendriya Meher <jitumeher018@gmail.com>
2017-09-23 10:46:02 +0530

    jitu

---
+++ src/main/java/com/spring/royallife/controller/IndexPageController.java
@@ -0,0 +1,20 @@
+package com.spring.royallife.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IndexPageController {
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<?> home() {
+		
+
+		return ResponseEntity.status(HttpStatus.OK).body("Welcome To Royal Life !");
+	}
+}
